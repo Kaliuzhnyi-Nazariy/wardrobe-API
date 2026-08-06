bccc012d8e630f807274413fccd0283f3d7b098e
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-08-06 23:28:48 +0200

    feat: implementing interfaces that are used accros whole project

--- interfaces.ts
+++ interfaces.ts
@@ -1,3 +1,10 @@
+import { Request } from "express";
+
 export interface IError extends Error {
-  status: number;
+  status?: number;
+  code?: number;
+}
+
+export interface CustomRequest extends Request {
+  user?: { id: string };
 }
